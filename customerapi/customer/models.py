1007698d4965bb896bb97906308972f5892352dc
Parameswari Bala <parameswaribala@gmail.com>
2024-02-12 16:01:54 +0530

    updated

--- customerapi/customer/models.py
+++ customerapi/customer/models.py
@@ -5,9 +5,12 @@ from django.db import models
 
 class Customer(models.Model):
     accountNo = models.IntegerField(primary_key=True)
-    firstName = models.TextField(null=False)
-    middleName = models.TextField(null=True)
-    lastName = models.TextField(null=False)
+    firstName = models.TextField(null=False, max_length=50)
+    middleName = models.TextField(null=True, max_length=50)
+    lastName = models.TextField(null=False, max_length=50)
+    contactNo = models.IntegerField(default=0)
+    email = models.TextField(max_length=150, null=False)
+    password = models.TextField(max_length=10, null=False)
 
 
 # class FullName:
@@ -17,8 +20,36 @@ class Customer(models.Model):
 
 class Address(models.Model):
     addressId = models.IntegerField(primary_key=True)
-    doorNo = models.TextField(null=False)
-    streetName = models.TextField(null=False)
-    city = models.TextField(null=False)
+    doorNo = models.TextField(null=False, max_length=5)
+    streetName = models.TextField(null=False, max_length=100)
+    city = models.TextField(null=False, max_length=100)
     pincode = models.IntegerField(default=0)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Gender(models.TextChoices):
+    MALE = 'MALE'
+    FEMALE = 'FEMALE'
+    TRANSGENDER = 'TRANSGENDER'
+
+
+class Individual(Customer):
+    gender = models.TextField(
+        choices=Gender.choices,
+        default=Gender.MALE
+    )
+    dob = models.DateField
+
+
+class CompanyType(models.TextChoices):
+    GOVERNMENT = 'GOVERNMENT'
+    PUBLIC = 'PUBLIC'
+    PRIVATE = 'PRIVATE'
+    NGO = 'NGO'
+
+
+class Corporate(Customer):
+    companyType = models.TextField(
+        choices=CompanyType.choices,
+        default=CompanyType.PRIVATE
+    )
---
+++ customerapi/customer/serializers.py
@@ -0,0 +1,22 @@
+from packaging.metadata import Metadata
+from rest_framework import serializers
+
+from customer.models import Customer, Individual, Corporate
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+class IndividualSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Individual
+        fields = '__all__'
+
+
+class CorporateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Corporate
+        fields = '__all__'
