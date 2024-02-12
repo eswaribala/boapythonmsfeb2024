419a24ca4214073f11d2d09822adbc06aeb91c15
Parameswari Bala <parameswaribala@gmail.com>
2024-02-12 15:22:38 +0530

    updated

--- customerapi/customer/models.py
+++ customerapi/customer/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Customer(models.Model):
+    accountNo = models.IntegerField(primary_key=True)
+    firstName = models.TextField(null=False)
+    middleName = models.TextField(null=True)
+    lastName = models.TextField(null=False)
+
+
+# class FullName:
+#     firstName = models.TextField(null=False)
+#     middleName = models.TextField(null=True)
+#     lastName = models.TextField(null=False)
+
+class Address(models.Model):
+    addressId = models.IntegerField(primary_key=True)
+    doorNo = models.TextField(null=False)
+    streetName = models.TextField(null=False)
+    city = models.TextField(null=False)
+    pincode = models.IntegerField(default=0)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
