8e73683e037b5e8bac313a52e5661b206d99e381
Parameswari Bala <parameswaribala@gmail.com>
2024-02-13 15:10:06 +0530

    updated

--- customerapi/customer_api/settings.py
+++ customerapi/customer_api/settings.py
@@ -10,6 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import hvac
+import sys
+
+# Authentication
+client = hvac.Client(
+    url='http://127.0.0.1:8200',
+    token='hvs.h4yQdfitEPcBeFzWH1o5VQFC',
+)
+
+# Writing a secret
+create_response = client.secrets.kv.v2.create_or_update_secret(
+     path='mysqlsecret',
+     secret=dict(password='vignesh',username='root'),
+ )
+
+print('Secret written successfully.')
+
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
