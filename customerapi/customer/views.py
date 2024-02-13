22a6e8de1243d5d3aaa2746bed1a144a7bdacaa2
Parameswari Bala <parameswaribala@gmail.com>
2024-02-13 10:15:04 +0530

    updated

--- customerapi/customer/views.py
+++ customerapi/customer/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -7,7 +9,26 @@ from customer.serializers import CustomerSerializer
 
 
 # Create your views here.
+@swagger_auto_schema(
+    methods=['post'],
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        required=['accountNo', 'firstName', 'middleName', 'lastName', 'contactNo', 'email', 'password'],
+        properties={
+            'accountNo': openapi.Schema(type=openapi.TYPE_NUMBER),
+            'firstName': openapi.Schema(type=openapi.TYPE_STRING),
+            'middleName': openapi.Schema(type=openapi.TYPE_STRING),
+            'lastName': openapi.Schema(type=openapi.TYPE_STRING),
+            'contactNo': openapi.Schema(type=openapi.TYPE_NUMBER),
+            'email': openapi.Schema(type=openapi.TYPE_STRING),
+            'password': openapi.Schema(type=openapi.TYPE_STRING),
+            # 'end_date': openapi.Schema(type=openapi.TYPE_STRING, default='yyyy-mm-dd'),
 
+        },
+    ),
+    operation_description='Create Customer',
+    responses={200: ""}
+)
 @api_view(['GET', 'POST'])
 def customer_data(request):
     if request.method == 'GET':
@@ -26,5 +47,3 @@ def customer_data(request):
             }
             return Response(response_data, status=201)
         return Response(serializer.errors, status=400)
-
-
