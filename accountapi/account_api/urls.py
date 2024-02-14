6176cedf204d1475d0aa4be25e6261cde40934e5
Parameswari Bala <parameswaribala@gmail.com>
2024-02-14 11:56:18 +0530

    updated

--- accountapi/account_api/urls.py
+++ accountapi/account_api/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+from account.views import account_data
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Account API",
+        default_version='v1', ),
+    public=True,
+    permission_classes=([permissions.AllowAny])
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/v1.0/', account_data),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
