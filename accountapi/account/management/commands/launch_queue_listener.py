19ba14957a954673c5fde052ce304ecb75b91b82
Parameswari Bala <parameswaribala@gmail.com>
2024-02-16 16:35:50 +0530

    updated

--- accountapi/account/management/commands/launch_queue_listener.py
+++ accountapi/account/management/commands/launch_queue_listener.py
@@ -4,9 +4,10 @@ from account.kafkaconsumerconfiguration import TransactionListener
 
 
 class Command(BaseCommand):
-    help = 'Launches Listener for user_created message : Kafka'
+    help = 'Launches Listener for transaction_created message : Kafka'
 
     def handle(self, *args, **options):
+        # thread instantiation
         td = TransactionListener()
         td.start()
         self.stdout.write("Started Consumer Thread")
--- transactionapi/transaction_api/settings.py
+++ transactionapi/transaction_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_swagger',
     'drf_yasg',
+    "django_elasticsearch_dsl",
 
 ]
 
