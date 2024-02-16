47d7f57d36cc537497056e6f8f5cc6a689dffefb
Parameswari Bala <parameswaribala@gmail.com>
2024-02-16 16:45:44 +0530

    updated

---
+++ transactionapi/transaction/documents.py
@@ -0,0 +1,25 @@
+from django_elasticsearch_dsl import Document, fields
+from django_elasticsearch_dsl.registries import registry
+
+from transaction.models import Transaction
+
+
+@registry.register_document()
+class TransactionDocument(Document):
+
+    class Index:
+        name = "transactions"
+        settings = {
+            "number_of_shards": 1,
+            "number_of_replicas": 0,
+        }
+
+    class Django:
+        model = Transaction
+        fields = [
+            "transactionId",
+            "amount",
+            "dot"
+        ]
+
+
